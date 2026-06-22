7d58ed225c631815fc48fa619121c90e148027c9
Kashvi Dashore <kashvidashore2610@gmail.com>
2026-06-23 00:21:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -118,16 +118,155 @@ def analyze_image(image_path):
 
     return result
 
+def save_json_report(result):
+
+```
+with open(
+    "output/result.json",
+    "w"
+) as f:
+
+    json.dump(
+        result,
+        f,
+        indent=4
+    )
+```
+
+def save_evidence_image(image_path):
+
+```
+results = rider_model(
+    image_path,
+    conf=0.4
+)
+
+annotated = results[0].plot()
+
+cv2.imwrite(
+    "output/evidence.jpg",
+    annotated
+)
+```
+
+def generate_pdf_report(result):
+
+```
+from reportlab.platypus import (
+    SimpleDocTemplate,
+    Paragraph,
+    Spacer
+)
+
+from reportlab.lib.styles import (
+    getSampleStyleSheet
+)
+
+pdf_path = "output/Violation_Report.pdf"
+
+doc = SimpleDocTemplate(pdf_path)
+
+styles = getSampleStyleSheet()
+
+content = []
+
+content.append(
+    Paragraph(
+        "TRAFFIC VIOLATION REPORT",
+        styles["Title"]
+    )
+)
+
+content.append(
+    Spacer(1, 12)
+)
+
+content.append(
+    Paragraph(
+        f"<b>Vehicle Type:</b> {result['vehicle_type']}",
+        styles["Normal"]
+    )
+)
+
+content.append(
+    Spacer(1, 10)
+)
+
+content.append(
+    Paragraph(
+        f"<b>Plate Number:</b> {result['plate_number']}",
+        styles["Normal"]
+    )
+)
+
+content.append(
+    Spacer(1, 10)
+)
+
+content.append(
+    Paragraph(
+        "<b>Detected Violations:</b>",
+        styles["Normal"]
+    )
+)
+
+for violation in result["violations"]:
+
+    content.append(
+        Paragraph(
+            f"• {violation}",
+            styles["Normal"]
+        )
+    )
+
+doc.build(content)
+```
+
+
+import os
 import sys
 
-if __name__ == "__main__":
+if **name** == "**main**":
 
-    if len(sys.argv) != 2:
-        print("Usage: python app.py <image_path>")
-        exit()
+```
+if len(sys.argv) != 2:
 
-    image_path = sys.argv[1]
+    print(
+        "Usage: python app.py <image_path>"
+    )
 
-    result = analyze_image(image_path)
+    exit()
 
-    print(json.dumps(result, indent=4))
+os.makedirs(
+    "output",
+    exist_ok=True
+)
+
+image_path = sys.argv[1]
+
+result = analyze_image(
+    image_path
+)
+
+save_json_report(result)
+
+save_evidence_image(
+    image_path
+)
+
+generate_pdf_report(
+    result
+)
+
+print(
+    json.dumps(
+        result,
+        indent=4
+    )
+)
+
+print(
+    "\nOutputs saved in output/"
+)
+```
+
